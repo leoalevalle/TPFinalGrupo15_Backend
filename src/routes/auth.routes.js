b8c084119311ea52948ca18c87148af0e3f10d3f
sofiii3 <cabanasofia440@gmail.com>
2026-07-08 17:23:14 -0300

    fix: correccion de registro de usuario y conductoras

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,7 @@ const authCtrl = require("../controllers/auth.controller");
 
 // POST /api/auth/login
 router.post("/login", authCtrl.iniciarSesion);
+router.post('/register', authCtrl.registrarPasajera);
+router.post('/register-conductora', authCtrl.registrarConductora);
 
 module.exports = router;
